0146144535b70e40021a53dc7ff9f4136c1fd5fe
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-08 12:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstChar(text) {
   // your code here
+	let trimmedText = text.trim();
+	if(trimmedText ){
+		return trimmedText[0]
+	}else if(!trimmedText){
+		return " ";
+	}
+
 }
 
 // Do not change the code below
